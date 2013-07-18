86c99c062645ac98f3fef26c149dbb30de1a889c
Johannes Walter <johannes@wltr.at>
2013-07-18 13:16:27 +0200

    Update o7sim.tcl

--- o7sim.tcl
+++ o7sim.tcl
@@ -2,6 +2,7 @@
 
 #------------------------------------------------------------------------------
 # o7sim - Mentor Graphics ModelSim Simulation Script
+# Copyright (c) 2013 Johannes Walter
 # <http://github.com/wltr/eda-o7sim>
 #
 # Version:
